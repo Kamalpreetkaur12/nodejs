277b7c807ca65e08c700b30b961d701f87f86289
Kamal <dearkamalpreet@gmail.com>
2022-11-10 11:56:45 +0100

    initial commit

---
+++ routers/userRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  addNewUser,
+  deleteUserById,
+  getAllUsers,
+  getOneUser,
+  updateUser,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.route("/").get(getAllUsers).post(addNewUser); // localhost:5000/users/
+
+//https://usersapp.onrender.com/users
+
+router.route("/:id").get(getOneUser).put(updateUser).delete(deleteUserById); // localhost:5000/users/:id
+export default router;
---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import userRouter from "./routers/userRouter.js";
+// import {errorHandler} from "./middleware/";
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello there");
+});
+app.use("/users", userRouter);
+
+// app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(" 😀 the server is run on port " + PORT);
+});
